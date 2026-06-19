2103f059136fc9de40ebf20978e17830ebc439ca
kingclaude123654-ux <kingclaude123654@gmail.com>
2026-06-19 04:40:51 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/medgulf/hse/MainActivity.java
+++ android/app/src/main/java/com/medgulf/hse/MainActivity.java
@@ -1,18 +1,83 @@
 package com.medgulf.hse;
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
+    private Uri cameraImageUri;
+    private static final int FILE_CHOOSER_REQUEST = 1;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected String getMainComponentName() {
-        return "medgulfhse";
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView wv, ValueCallback<Uri[]> cb, FileChooserParams p) {
+                filePathCallback = cb;
+                openChooser();
+                return true;
+            }
+        });
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    private void openChooser() {
+        Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            File f = File.createTempFile("HSE_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date()), ".jpg", getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+            cameraImageUri = FileProvider.getUriForFile(this, "com.medgulf.hse.fileprovider", f);
+            cam.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+        } catch (IOException e) { e.printStackTrace(); }
+        Intent gal = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent chooser = Intent.createChooser(gal, "Select Image");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{cam});
+        startActivityForResult(chooser, FILE_CHOOSER_REQUEST);
     }
 
     @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new DefaultReactActivityDelegate(this, getMainComponentName(), false);
+    protected void onActivityResult(int req, int res, Intent data) {
+        super.onActivityResult(req, res, data);
+        if (req != FILE_CHOOSER_REQUEST || filePathCallback == null) return;
+        Uri[] results = null;
+        if (res == Activity.RESULT_OK) {
+            results = (data == null || data.getData() == null) ? new Uri[]{cameraImageUri} : new Uri[]{data.getData()};
+        }
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 }
